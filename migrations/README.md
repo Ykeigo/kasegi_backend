20ea9bdc99f3d5aa5fbb4481ff867bce9368bdf0
Ykeigo <yanagirts@gmail.com>
2023-08-05 14:36:52 +0000

    insertが動くことを確認

--- migrations/README.md
+++ migrations/README.md
@@ -1,7 +1,7 @@
 postgleSQLのパスをexportしておく
 postgressqlのURL（？）はこうらしい
 一番最初のpostgres://っていうのはどういうこと？httpとかじゃなくてpostgres形式で通信します的なこと？
-export POSTGRESQL_URL='postgres://postgres:password@localhost:5432/${DB名}sslmode=disable'
+export POSTGRESQL_URL='postgres://postgres:password@localhost:5432/${DB名}?sslmode=disable'
 
 バージョンアップ
 migrate -database $POSTGRESQL_URL -path migrations/example1 up ${上げるバージョン数（指定しない場合全部）}
--- src/insertTest.go
+++ src/insertTest.go
@@ -11,9 +11,13 @@ import (
 )
 
 func main() {
+	// https://qiita.com/hiro9/items/e6e41ec822a7077c3568
 	db, _ := sql.Open(
 		"postgres",
-		"host=postgres dbname=example user=postgres password=password sslmode=disable")
+		"host=localhost port=5432 dbname=example user=postgres password=password sslmode=disable")
 	titles, _ := models.GameTitles().All(context.Background(), db)
 	fmt.Println("jets:", titles)
+	for _, v := range titles {
+		fmt.Println(v)
+	}
 }
