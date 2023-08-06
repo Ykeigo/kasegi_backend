638e1ec95880e9d2fdac8987c5be72c4b6dc85f2
Ykeigo <yanagirts@gmail.com>
2023-08-06 09:02:34 +0000

    mainに全部つめこむ

--- src/main.go
+++ src/main.go
@@ -5,12 +5,14 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/gin-gonic/gin"
+
 	models "test-app/models_psql"
 
 	_ "github.com/lib/pq" //なんかよくわからんけどいる　ないとruntimeErrorを吐く
 )
 
-func main() {
+func insertTest() {
 	// https://qiita.com/hiro9/items/e6e41ec822a7077c3568
 	db, _ := sql.Open(
 		"postgres",
@@ -20,4 +22,25 @@ func main() {
 	for _, v := range titles {
 		fmt.Println(v)
 	}
+}
+
+func webServerTest() {
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+
+	})
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+	})
+	r.Run(":80")
+}
+
+func main() {
+	webServerTest()
 }
